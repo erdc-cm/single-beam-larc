449c800e969544cc6e6a4c4a11542608f3944652
cekees <cekees@gmail.com>
2014-08-11 18:05:19 -0500

    refactored survey.py and created  demo notebook

--- survey.py
+++ survey.py
@@ -1,61 +1,80 @@
-def survey_row_to_dict(survey_row):
-    """Converts a row of survey to dictionary
+"""
+A module for processing survey data
 
-    extracts survey id, line number, x, and z coordinates
+This module collects implements algorithms for  reading and  processing surveys.
+"""
+import numpy as np
+
+class Survey:
     """
-    row_list = survey_row.split(',')
-    row_dict = {}
-    row_dict['id'] = int(row_list[0])
-    row_dict['line_number'] = int(row_list[1])
-    row_dict['x'] = float(row_list[7])
-    row_dict['z'] = float(row_list[9])
-    return row_dict
-
-
-class SurveyDict:
-    def __init__(self):
+    A survey of a specific site with multiple survey lines
+    """
+    def __init__(self,fname):
+        import numpy as np
         self.lines = {}
+        self.lines_ll = {}
+        self.csvArray = csvArray = np.genfromtxt(fname=fname,delimiter=',',skip_header=0,
+                                            names=('id'    ,'line_number' ,'lon'      ,'lat'    ,'x'       ,'y'       ,'z'),
+                                            dtype=(np.uint ,np.uint       ,np.double, np.double ,np.double ,np.double ,np.double),
+                                            usecols=(0,1,3,4,7,8,9))
+        for line_number in set(self.csvArray['line_number']):
+            self.lines[line_number] = np.transpose(np.vstack([self.csvArray['x'][csvArray['line_number'] == line_number],
+                                                              self.csvArray['y'][csvArray['line_number'] == line_number],
+                                                              self.csvArray['z'][csvArray['line_number'] == line_number]]))
+            self.lines_ll[line_number] = np.transpose(np.vstack([self.csvArray['lon'][csvArray['line_number'] == line_number],
+                                                                 self.csvArray['lat'][csvArray['line_number'] == line_number],
+                                                                 self.csvArray['z'][csvArray['line_number'] == line_number]]))
+    def  plot_lines(self,line_numbers=None):
+        from mpl_toolkits.mplot3d import Axes3D
+        import matplotlib.pyplot as plt
+        fig = plt.figure(figsize=(12,12))
+        ax = fig.add_subplot(111, projection='3d')
+        if line_numbers==None:
+            line_numbers = self.lines.keys()
+        for  ln in line_numbers:
+            ax.scatter(self.lines[ln][:,0],self.lines[ln][:,1],self.lines[ln][:,2],c=self.lines[ln][:,2])
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        return plt
 
-    def insert_row(self, survey_row):
-        row_dict = survey_row_to_dict(survey_row)
-        line_number = row_dict['line_number']
-        value = row_dict['x'], row_dict['z']
-        if line_number in self.lines:
-            # The line number has already been added
-            self.lines[line_number].append(value)
-        else:
-            # We need to create this line
-            self.lines[line_number] = [value]
+    def plot_surface(self):
+        from mpl_toolkits.mplot3d import Axes3D
+        from matplotlib import cm
+        import matplotlib.pyplot as plt
+        import numpy as np
+        fig = plt.figure(figsize=(12,12))
+        ax = fig.gca(projection='3d')
+        ax.plot_trisurf(self.csvArray['x'],self.csvArray['y'],self.csvArray['z'], cmap=cm.jet, linewidth=0.0)
+        return plt
 
+    def plot_contours(self):
+        import matplotlib.pyplot as plt
+        import numpy as np
+        plt.figure(figsize=(12,12))
+        plt.tricontourf(self.csvArray['x'],self.csvArray['y'],self.csvArray['z'])
+        return plt
+    
+    def kml(self,fname):
+        import simplekml
+        kml = simplekml.Kml()
+        for x,y,z in zip(self.csvArray['lon'],self.csvArray['lat'],self.csvArray['z']):
+            kml.newpoint(coords=[(x,y,z)])  # lon, lat, optional height
+        kml.save(fname+".kml")
 
-def test_survey_row_to_dict():
-    import nose.tools
-
-    test_row1 = '39,165,754,36.182174440,-75.750553152,901857.088,274598.202,83.966,506.940,1.813,-37.055,20121127,121708,44228.418\r\n'
-    test_row2 =  '39,165,754,36.182179682,-75.750529447,901859.201,274598.853,86.173,506.829,1.616,-37.252,20121127,121707,44227.418\r\n'
-
-    d1 = survey_row_to_dict(test_row1)
-    test_d1 = {'id': 39, 'line_number': 165, 'x': 83.966, 'z': 1.813}
-    nose.tools.eq_(d1, test_d1)
-
-    d2 = survey_row_to_dict(test_row2)
-    test_d2 = {'id': 39, 'line_number': 165, 'x': 86.173, 'z': 1.616}
-    nose.tools.eq_(d2, test_d2)
-
-def test_survey_class():
-    import nose
-
-    test_row1 = '39,165,754,36.182174440,-75.750553152,901857.088,274598.202,83.966,506.940,1.813,-37.055,20121127,121708,44228.418\r\n'
-    test_row2 =  '39,165,754,36.182179682,-75.750529447,901859.201,274598.853,86.173,506.829,1.616,-37.252,20121127,121707,44227.418\r\n'
-
-    sd = SurveyDict()
-    test_sd_empty = {}
-    nose.tools.eq_(sd.lines, test_sd_empty)
-
-    sd.insert_row(test_row1)
-    test_sd1 = [(83.966, 1.813)]
-    nose.tools.eq_(sd.lines[165], test_sd1)
-
-    sd.insert_row(test_row2)
-    test_sd2 = [(83.966, 1.813), (86.173, 1.616)]
-    nose.tools.eq_(sd.lines[165], test_sd2)
+class SurveySite:
+    """
+    A specific site with (overlapping) surveys through time
+    """
+    def __init__(self,dirname):
+        """
+        Read all the csv files in a directory
+        """
+        import glob,os
+        self.surveys=[]
+        for f in glob.glob(os.path.join(dirname,"*.csv")):
+            self.surveys.append(Survey(fname=f))
+    def contourAnimation(self):
+        for si,s in enumerate(self.surveys):
+            contour_plot = s.plot_contours()
+            contour_plot.savefig("contour%3.3d"  % (si,))
